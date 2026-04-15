6b5c4456aaca22a6a2f89ee331a9091335f5ddce
Radosław Szkoda <radiszko@RadiszkoMacBookM5Max.local>
2026-04-15 11:42:02 +0200

    fix: naprawa kompilacji - dodanie brakującej paczki Logging.Configuration
    - dodano Microsoft.Extensions.Logging.Configuration do csproj
    - naprawiono błąd CS1929 w Program.cs

--- FCOldBoysTeamSelector/Program.cs
+++ FCOldBoysTeamSelector/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Web;
 using FCOldBoysTeamSelector.Services;
 using FCOldBoysTeamSelector.Components;
+using Microsoft.Extensions.Logging;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,7 +11,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<TeamGeneratorService>();
 builder.Services.AddScoped<LocalStorageService>();
+
+// Rejestracja SupabaseService z konfiguracją
+builder.Services.Configure<SupabaseServiceOptions>(
+    builder.Configuration.GetSection("Supabase"));
 builder.Services.AddScoped<SupabaseService>();
+
+builder.Logging.AddConfiguration(
+    builder.Configuration.GetSection("Logging"));
 
 await builder.Build().RunAsync();
 
